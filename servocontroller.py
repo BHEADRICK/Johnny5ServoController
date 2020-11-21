2e6ca9ebcd7b26789b189df10b6007a394ff7fe4
Bryan Headrick <>
2020-11-20 21:37:24 -0500

    update

--- servocontroller.py
+++ servocontroller.py
@@ -39,16 +39,20 @@ servos = {
 	},
 	'leftBrowRotate': {
 	'id':10,
-	'home': 80,
+	'home': 20,
 	'counter': False,
-	'current': 80,
-	'min': 60,
-	'max': 160
+	'current': 20,
+	'min': 15,
+	'max': 130
 	},
 	'noseRotate': {
 	'id':12,
-	'home': 80,
-	'counter': False
+	'home': 30,
+	'current':30,
+	'counter': False,
+	'min':0,
+	'max':80,
+
 	},
 	'nosePivot': {
 	'id':11,
@@ -58,44 +62,51 @@ servos = {
 	'min': 60,
 	'max': 110
 	},
-	'rightEyeX': {
+	'leftEyeX': {
 	'id': 0,
 	'home': 80,
 	'current': 80,
-	'min': 45,
-	'max': 125,
+	'min':0 ,
+	'max': 160,
 	'counter': True
 	},
-	'rightEyeY': {
+	'leftEyeY': {
 	'id': 1,
-	'home': 45,
-	'current': 45,
+	'home': 30,
+	'current': 30,
 	'min': 0,
-	'max': 110,
+	'max': 60,
 	'counter': False
 	},
-	'rightEyeIris':{
+	'leftEyeIris':{
 	'id':2,
 	'home':0,
 	'current':0,
-	'counter':True
+	'counter':True,
+	'min':0,
+	'max':50
+	},
+	'rightLowerEyeFlap': {
+	'id':9,
+	'home': 180,
+	'current': 180,
+	'min': 70,
+	'max':180,
+	'counter': True
+	},
+	'leftLowerEyeFlap': {
+	'id':8,
+	'home': 0,
+	'current': 0,
+	'min': 0,
+	'max':90
 	}
 }
 
 
-# kit.servo[servo].angle = 60
-# sleep(1)
-# kit.servo[servo].angle = 80
-# sleep(1)
-	# kit.servo[servo].angle = 60
-#
-# kit.servo[4].angle = 80
-# kit.servo[5].angle = 80
-#
-# kit.servo[6].angle = 80
-# kit.servo[7].angle = 80
 
 def homeAll():
+	print('homing...')
 	for key in servos:
 		kit.servo[servos[key]['id']].angle = servos[key]['home']
 		sleep(.1)
@@ -128,94 +139,66 @@ def moveServo(servoKey, angle, smooth = False):
 		dest = servo.min
 	if dest>servo.max:
 		dest = servo.max
-	print("moving servo to " + str(dest))
+	print("moving servo" + servoKey + " to " + str(dest))
 	if smooth:
 		smoothAngle(servo.id, dest, servo.current)
 	else:
 		kit.servo[servo.id].angle = dest
 	servos[servoKey]['current'] = dest
+def blinkWink():
 
-# import rospy
-# from std_msgs.msg import Float32
+	moveServo('rightBrowTilt', -90)
+	moveServo('leftBrowTilt', -90)
+	moveServo('rightLowerEyeFlap', 90)
+	moveServo('leftLowerEyeFlap', 90)
+
+	sleep(1)
+	moveServo('rightBrowTilt', 90)
+	moveServo('leftBrowTilt', 90)
+	moveServo('rightLowerEyeFlap', -90)
+	moveServo('leftLowerEyeFlap', -90)
+	sleep(1)
+
+	moveServo('rightBrowTilt', -90)
+	moveServo('rightLowerEyeFlap', 90)
+
+	sleep(1)
+	moveServo('rightBrowTilt', 90)
+	moveServo('rightLowerEyeFlap', -90)
+
+	sleep(1)
+	moveServo('leftLowerEyeFlap', 90)
+	moveServo('leftBrowTilt', -90)
+	sleep(1)
+
+	moveServo('leftBrowTilt', 90)
+	moveServo('leftLowerEyeFlap', -90)
+	sleep(2)
+
+def testRange(srv, start, inc=5):
+
+	angle = start
+	kit.servo[srv].angle = angle
+	sleep(1)
+	# kit.continuous_servo[servo].throttle = 1
+	# try:
+	while angle >=0 and angle <= 180:
+
+		print("moving to angle: " + str( angle))
+		kit.servo[srv].angle = angle
+		angle +=inc
+		sleep(1)
+
 
 homeAll()
-# sleep(3)
-# moveServo('nosePivot', 30)
-# sleep(1)
-# moveServo('rightEyeX', 40)
-# sleep(1)
-# moveServo('rightEyeX', -80)
-# sleep(1)
-# moveServo('rightEyeY', 50)
-# sleep(1)
-# moveServo('rightEyeY', -100)
-# sleep(1)
-# moveServo('rightBrowRotate',15)
-# moveServo('leftBrowRotate',15)
-# sleep(.2)
-# moveServo('rightBrowRotate',-15)
-# moveServo('leftBrowRotate',-15)
-# sleep(.2)
-# moveServo('rightBrowRotate',15)
-# moveServo('leftBrowRotate',15)
-# sleep(.2)
-# moveServo('rightBrowRotate',-15)
-# moveServo('leftBrowRotate',-15)
-# # #
-# sleep(3)
-# homeAll()
+# testRange(2,80,5)
+sleep(1)
+moveServo('leftBrowTilt',30)
+moveServo('rightBrowTilt',30)
+moveServo('nosePivot',10)
+moveServo('leftEyeY',60)
+moveServo('noseRotate',30)
+moveServo('leftLowerEyeFlap',30)
+moveServo('rightLowerEyeFlap',30)
 
-# kit.servo[1].angle = 80
-#
-# servo = 1
-# angle = 80
-# try:
-# 	while angle >0:
-# 		angle -=5
-# 		print("moving to angle: " + str( angle))
-# 		kit.servo[servo].angle = angle
-#
-# 		sleep(1)
-# #
-# except KeyboardInterrupt:
-# 	kit.continuous_servo[servo].throttle = 0
-
-# def func1():
-# 	smoothAngle(6,120,60)
-
-# def func2():
-# 	smoothAngle(7,120,60)
-
-# p1 = Process(target = func1)
-# p2 = Process(target = func2)
-# p1.start()
-# p2.start()
-
-#
-#
-
-# print("jerky movement")
-# sleep(1)
-# kit.servo[1].angle=60
-# sleep(1)
-# kit.servo[1].angle=0
-# sleep(2)
-
-# print("slow movement")
-# setAngle(1,60, 50)
-# sleep(1)
-# kit.servo[1].angle=0
-# sleep(2)
-
-# print("smooth movement")
-# smoothAngle(1,60)
-# sleep(1)
-# smoothAngle(1,0,60)
-
-# sleep(3)
-# smoothAngle(2,50,80)
-# smoothAngle(3, 110, 80)
-
-# sleep(2)
-# smoothAngle(4,60,80)
-# smoothAngle(5,60,80)
+# moveServo('leftBrowRotate', 0, True)
